0947a1616981e6522f891075eb896df777ece21a
lvjinyue <jinyue.lv@ichezheng.com>
2016-04-07 10:12:51 +0800

    add ord_chr.py

--- print_function_and_strings.py
+++ print_function_and_strings.py
@@ -10,4 +10,28 @@ print(int('8')+5)
 print('Hi', 'hello')
 print('100 + 200 =', 100 + 200)
 name = input()
-print(name)
+print('hello', name)
+name = input('Please enter your name: ')
+print('hello', name)
+print('\\\n\\')
+print('''
+line1
+    line2
+line3
+            line4
+''')
+age = int(input('please enter you age: '))
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+
+print('''
+n = 123
+f = 456.789
+s1 = \'Hello, world\'
+s2 = \'Hello, \\\'Adam\\\'\'
+s3 = r\'Hello, "Bart"\'
+s4 = r\'\'\'Hello,
+Lisa!\'\'\'
+''')
