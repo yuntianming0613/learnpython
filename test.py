fb5bd26a4045735ec99a4a8249f7cc956b626a73
lvjinyue <jinyue.lv@ichezheng.com>
2016-04-08 18:14:58 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# import sys
+# from collections import deque
+# print(sys.platform)
+# print(2 ** 100)
+# x = 'Spam!'
+# print(x * 8)
+#
+# queue = deque(['Eric', 'John', 'Michael'])
+# queue.append('Terry')
+# queue.append('Graham')
+# print(queue)
+# print(queue.popleft())
+# print(queue.popleft())
+# print(queue)
+#
+# mat = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print([[row[i] for row in mat] for i in [0, 1, 2]])
+#
+# for i in [0, 1, 2]:
+#     for row in mat:
+#         print(row[i], end='')
+#     print()
+
+# print(list(zip(*mat)))
+#
+# a = [-1, 1, 66.25, 333, 333, 1234.5]
+# print(a)
+# del a[0]
+# print(a)
+# del a[2:4]
+# print(a)
+# del a[:]
+# print(a)
+# del a
+
+t = 12345, 54321, 'hello'
+print(t[0])
+print(t)
+u = (t, (1, 4444))
+print(u)
+
+
+s1 = [12, 23, 34]
+s2 = [22, 23, 24]
+print(s1, s2)
+print(zip(s1, s2))
+
+
+
+
+
+
